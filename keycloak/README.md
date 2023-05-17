9c0ff7d1dbf6a66612a1cd213d2b1d07906801ac
verpul <nicenick@mail.ru>
2023-05-17 16:22:34 +0300

    v 6.0 Docker

---
+++ keycloak/README.md
@@ -0,0 +1,7 @@
+## For local testing:
+add 127.0.0.1 keycloak to your /etc/hosts file
+
+### Default user
+admin@admin.ru
+
+123456
--- weight/src/main/java/ru/verpul/weight/config/SecurityConfig.java
+++ weight/src/main/java/ru/verpul/weight/config/SecurityConfig.java
@@ -1,16 +1,9 @@
 package ru.verpul.weight.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 public class SecurityConfig {
