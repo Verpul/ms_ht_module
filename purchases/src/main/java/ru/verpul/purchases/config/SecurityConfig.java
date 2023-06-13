619d18dd4e09b07afbb3b143061500110d849780
verpul <nicenick@mail.ru>
2023-06-13 16:13:32 +0300

    v 5.3 Updated for Purchases module

--- purchases/src/main/java/ru/verpul/purchases/config/SecurityConfig.java
+++ purchases/src/main/java/ru/verpul/purchases/config/SecurityConfig.java
@@ -1,21 +1,21 @@
-package ru.verpul.purchases.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/purchases/**").authenticated()
-                )
-                .oauth2ResourceServer()
-                .jwt();
-        return http.build();
-    }
-}
+//package ru.verpul.purchases.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers("/purchases/**").authenticated()
+//                )
+//                .oauth2ResourceServer()
+//                .jwt();
+//        return http.build();
+//    }
+//}
--- vue/src/main.js
+++ vue/src/main.js
@@ -2,47 +2,53 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import Keycloak from "keycloak-js";
-import setHeader from "@/service/HeaderService";
+// import Keycloak from "keycloak-js";
+// import setHeader from "@/service/HeaderService";
 
 Vue.config.productionTip = false
 
-let initOptions = {
-  url: process.env.VUE_APP_KEYCLOAK_URL,
-  realm: process.env.VUE_APP_KEYCLOAK_REALM,
-  clientId: process.env.VUE_APP_CLIENT_ID,
-  onLoad: process.env.VUE_APP_ON_LOAD
-}
+// let initOptions = {
+//   url: process.env.VUE_APP_KEYCLOAK_URL,
+//   realm: process.env.VUE_APP_KEYCLOAK_REALM,
+//   clientId: process.env.VUE_APP_CLIENT_ID,
+//   onLoad: process.env.VUE_APP_ON_LOAD
+// }
 
-let keycloak = new Keycloak(initOptions);
+// let keycloak = new Keycloak(initOptions);
+//
+// keycloak.init({onLoad: initOptions.onLoad}).then((auth) => {
+//   if (!auth) {
+//     window.location.reload();
+//   } else {
+//     setHeader(keycloak.token);
+//
+//     new Vue({
+//       router,
+//       vuetify,
+//       render: h => h(App)
+//     }).$mount('#app');
+//
+//     //For logout
+//     Vue.prototype.$keycloak = keycloak
+//   }
+//
+//   //Token refresh
+//   setInterval(() => {
+//     keycloak.updateToken(1200).then((refreshed) => {
+//       if (refreshed) setHeader(keycloak.token);
+//     }).catch(() => {
+//       console.log('Failed to refresh token');
+//     });
+//   }, 60000)
+//
+// }).catch(() => {
+//   console.log("Authenticated Failed");
+// });
 
-keycloak.init({onLoad: initOptions.onLoad}).then((auth) => {
-  if (!auth) {
-    window.location.reload();
-  } else {
-    setHeader(keycloak.token);
-
-    new Vue({
-      router,
-      vuetify,
-      render: h => h(App)
-    }).$mount('#app');
-
-    //For logout
-    Vue.prototype.$keycloak = keycloak
-  }
-
-  //Token refresh
-  setInterval(() => {
-    keycloak.updateToken(1200).then((refreshed) => {
-      if (refreshed) setHeader(keycloak.token);
-    }).catch(() => {
-      console.log('Failed to refresh token');
-    });
-  }, 60000)
-
-}).catch(() => {
-  console.log("Authenticated Failed");
-});
+new Vue({
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app');
 
 
